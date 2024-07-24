75f2bb3bc0a0e3a4e196f81e33aee7b5e0f45fed
cristian Ruiz <cristianwwe@hotmail.com>
2024-07-24 13:52:50 -0500

    modificaciones, cambio de algunas imagenes y eventos futuros modificado

---
+++ upcomongevents.js
@@ -0,0 +1,28 @@
+console.log("aqui se muestran eventos próximos");
+
+const eventos = datos.eventos;
+const fechaActual = new Date(datos.fechaActual); // "2023-01-01"
+
+function displayEvents(eventos) {
+    const container = document.getElementById("events-container");
+    container.innerHTML = "";
+
+    eventos
+        .filter(evento => new Date(evento.fecha) > fechaActual)
+        .forEach(evento => {
+            const eventoHTML = `
+                <div class="col-md-3 col-sm-6 mb-4">
+                    <div class="card">
+                        <img src="${evento.imagen}" class="card-img-top" alt="${evento.nombre}">
+                        <div class="card-body">
+                            <h2 class="card-title">${evento.nombre}</h2>
+                            <a href="details.html?id=${evento._id}" class="btn btn-primary">Detalles</a>
+                        </div>
+                    </div>
+                </div>
+            `;
+            container.innerHTML += eventoHTML;
+        });
+}
+
+displayEvents(eventos);
